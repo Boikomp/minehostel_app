6479b0e322e8d7316f68f579ebbc23caa6653d7b
Maksim Boiko <max.asus1742@gmail.com>
2024-01-29 17:19:25 +0300

    Добавил форму и функцию добавления бронирования

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from datetime import datetime
 from enum import Enum
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import (DateField, IntegerField, StringField, SubmitField,
+                     TextAreaField)
+from wtforms.validators import DataRequired, Length
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'sefj;4is4b4;j4;4h;oishtho4;otih4;thi'
 db = SQLAlchemy(app)
 
 
@@ -43,15 +48,57 @@ class Order(db.Model):
                 f'статус - {self.status}')
 
 
+class OrderForm(FlaskForm):
+    name = StringField(
+        'Имя гостя',
+        validators=[DataRequired(message='Обязательное поле'),
+                    Length(1, 100)]
+    )
+    guests_count = IntegerField(
+        'Количество гостей',
+        validators=[DataRequired(message='Обязательное поле')]
+    )
+    checkin_date = DateField(
+        'Дата заезда (ГГГГ-ММ-ДД)',
+        format='%Y-%m-%d',
+        validators=[DataRequired(message='Обязательное поле')]
+    )
+    checkout_date = DateField(
+        'Дата выезда (ГГГГ-ММ-ДД)',
+        format='%Y-%m-%d',
+        validators=[DataRequired(message='Обязательное поле')]
+    )
+    price = IntegerField(
+        'Стоимость одной ночи',
+        validators=[DataRequired(message='Обязательное поле')]
+    )
+    comment = TextAreaField('Комментарий')
+    submit = SubmitField('Создать')
+
+
 @app.route('/')
 def index():
     orders = Order.query.all()
     return render_template('index.html', orders=orders)
 
 
-@app.route('/order-create')
+@app.route('/order-create', methods=['GET', 'POST'])
 def order_create():
-    return render_template('order_create.html')
+    form = OrderForm()
+    if form.validate_on_submit():
+        order = Order(
+            name=form.name.data,
+            guests_count=form.guests_count.data,
+            checkin_date=form.checkin_date.data,
+            checkout_date=form.checkout_date.data,
+            price=form.price.data,
+            comment=form.comment.data,
+        )
+        db.session.add(order)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    return render_template('order_create.html', form=form)
 
 
 if __name__ == '__main__':
