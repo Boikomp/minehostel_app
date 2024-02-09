f5ad38070075030a3510b74f63b7add8f602421a
Maksim Boiko <max.asus1742@gmail.com>
2024-02-09 10:32:31 +0300

    Разбил шаблоны по папкам

--- reservations_app/error_handlers.py
+++ reservations_app/error_handlers.py
@@ -7,10 +7,12 @@ from . import app, db
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'), HTTPStatus.NOT_FOUND
+    return (render_template('error_handlers/404.html'),
+            HTTPStatus.NOT_FOUND)
 
 
 @app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
-    return render_template('500.html'), HTTPStatus.INTERNAL_SERVER_ERROR
+    return (render_template('error_handlers/500.html'),
+            HTTPStatus.INTERNAL_SERVER_ERROR)
