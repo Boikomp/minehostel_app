a8f950c97aa6a61c590d71d9cc421028127520e9
Maksim Boiko <max.asus1742@gmail.com>
2024-02-19 11:39:51 +0300

    Импортировал модели

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 auth_bp = Blueprint('auth', __name__)
 
-from . import cli_commands, routes
+from . import cli_commands, models, routes
--- app/reservations/__init__.py
+++ app/reservations/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 reservations_bp = Blueprint('reservations', __name__)
 
-from . import routes
+from . import models, routes
